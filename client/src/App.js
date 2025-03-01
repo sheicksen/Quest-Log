ae919c6bdd11df96e1ca9514fb1dafbbfef913cf
Sophia Heicksen <heicksen@udel.edu>
2025-03-01 12:19:32 -0500

    Made server-client data transfer possible with Axiom.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+// Gets data sent by the home route.
 const apiCall = () => {
-  axios.get('http://localhost:8080').then((data) => {
+  axios.get('http://localhost:8080/').then((data) => {
     console.log(data);
   });
 }
