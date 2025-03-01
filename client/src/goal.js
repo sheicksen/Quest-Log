d9eccf543db1a4961c927de9ad33f4f5ba101b05
Samantha Paez <sampaez@udel.edu>
2025-03-01 18:16:27 -0500

    goal visibility (scuffed) & can mark as complete

--- client/src/goal.js
+++ client/src/goal.js
@@ -7,12 +7,14 @@ export class GoalClass{
     repeatable
     streak
     complete
+    visible
     constructor(name, description, repeatable){
         this.name = name
         this.description = description
         this.repeatable = repeatable
-        this.streak = 0;
+        this.streak = 0
         this.complete = false
+        this.visible = false
     };
 };
 
@@ -22,21 +24,43 @@ export const Goal = () => {
 
     let [goalModalOpen, setGoalModalOpen] = useState(false);
 
+   let [visible, setVisible] = useState(false);
+
+   function flipVisibility() {
+        setVisible(!visible)
+   }
+
     function markComplete(goal){
        goal.complete = true;
     }
 
+    /*function flipVisibility(goal){
+        (goal.visible===true) ? goal.visible = false : goal.visible = t
+    }*/
 
     return (
         <div>
+            <div className="goal-list-modal">
             {goals.map(function(goal){
-                    return <li key={goal.name}>{goal.name}</li>
+                    return <div className="listed-goal">
+                        <button onClick={flipVisibility}>
+                        <li key={goal.name}>{goal.name}</li>
+                        </button>
+                        {visible && <div>
+                            <br></br>
+                            {goal.description}
+                            <br></br>
+                            Streak: {goal.streak}
+                            <br></br>
+                            <button onClick={markComplete(goal)}>Mark Completed</button>
+                        </div>}
+                    </div>
                   })}
+            </div>
             <button className="newGoal-button" onClick={() => setGoalModalOpen(true)}>Make New Goal</button>
             {console.log(goals)}
             {goalModalOpen && <NewGoalModal closeNewGoalModal={()=>{setGoalModalOpen(false)}}/>} 
             {
-                //<button onClick={markComplete}>Mark as complete</button>
             }  
         </div>
     )
