56f6c56c924b5566292e2996d6817a9ab5cf9c14
Sophia Heicksen <heicksen@udel.edu>
2025-03-01 11:45:57 -0500

    Hosting Homepage on port 8080

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const app = express();
+console.log(app);
+
+
+app.listen(8080, () => {
+    console.log('Server listening on port 8080');
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
