deacd625b432bbbcd8e9af47755d2cf7ad93d5d9
Sophia Heicksen <heicksen@udel.edu>
2025-03-01 16:54:36 -0500

    Edited Navbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,16 +1,11 @@
 import './App.css';
-import {Nav, Navbar} from 'react-bootstrap';
+import {Navbar} from 'react-bootstrap';
 import logo from "./images/logo192.png";
 
 function Navigation(){
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="App-nav">
         <Navbar.Brand href="#home"><img  height="30px" width="30px" src={logo}></img>Quest Log</Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-        {/* <Navbar.Collapse  id="basic-navbar-nav">
-            <Nav className="me-auto">
-            </Nav>
-        </Navbar.Collapse>       */}
     </Navbar>
     );
 }
