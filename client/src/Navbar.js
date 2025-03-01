5adbd6820ef25a703d30ce6a453c62635bc45911
nolanKeefe <npkeefe@udel.edu>
2025-03-01 17:03:45 -0500

    Colors

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,10 +1,10 @@
 import './App.css';
-import {Nav, Navbar} from 'react-bootstrap';
+import {Navbar} from 'react-bootstrap';
 import logo from "./images/logo192.png";
 
 function Navigation(){
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="App-nav">
         <Navbar.Brand href="#home"><img  height="30px" width="30px" src={logo}></img>Quest Log</Navbar.Brand>
         {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
         {/* <Navbar.Collapse  id="basic-navbar-nav">
