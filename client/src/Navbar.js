464de9b5971e0e3a0eb49a7d5036311364a9b848
nolanKeefe <npkeefe@udel.edu>
2025-03-02 08:01:02 -0500

    Text size changes but now slider dont move??

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,8 +1,16 @@
 import './App.css';
 import {Navbar,Nav,NavDropdown,Form} from 'react-bootstrap';
 import logo from "./images/Flower11.png";
+import {React,useState} from 'react';
+
 
 function Navigation(){
+    const [sliderValue, setSliderValue] = useState(50);
+
+    const handleSliderChange = (e) => {
+        setSliderValue(e.target.value);
+        document.documentElement.style.setProperty('--slider-value',`${e.target.value}px`)
+    };
     return (
         <Navbar expand="lg" className="App-nav">
         <Navbar.Brand href="#home" class="page-logo"><img  height="50px" width="50px" src={logo} class="pixel-art"></img>Quest Log</Navbar.Brand>
@@ -14,21 +22,12 @@ function Navigation(){
             <Nav.Link href="#home">Home</Nav.Link>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1" ><Form.Label>Text Size:</Form.Label>
-              <Form.Range /></NavDropdown.Item>
+              <Form.Range id = "customRange" value = '--sliderValue' onChange = {handleSliderChange} className="custom-slider"/></NavDropdown.Item>
+              <p>Selected Value: {sliderValue}</p>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
 
-    {/* <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="dropdown-menu" aria-labelledby="navbarDropdown">Settings</a>
-        <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-      </li>
-    </ul> */}
   </div>
     </Navbar>
     
