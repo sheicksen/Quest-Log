118d860ee35bd9f6f441804618d1bfef355143db
Sophia Heicksen <heicksen@udel.edu>
2025-03-02 04:28:44 -0500

    Began addressing bug in complete garden

--- client/src/completedGarden.js
+++ client/src/completedGarden.js
@@ -4,6 +4,7 @@ import Flower11 from './images/Flower11.png';
 import './App.css';
 
 export function CompletedGarden() {
+    console.log("Comp garden",completeNonrepeat.length >= 1? completeNonrepeat : "Not yet initialized");
     return (
         <div class="bg-green-gradient center-content">
             <h1>Garden of Success</h1>
@@ -12,7 +13,7 @@ export function CompletedGarden() {
                 {completeNonrepeat.map(((goal)=> 
                     <div >
                         <img class="pixel-art" height="100px" width="100px"  src={Flower11} alt="Your streak flower!"></img>
-                        {goal.name}
+                        <p>{goal.name}</p>
                     </div>
                 ))}
             </div>
