b605af9919c6141acd2f11b3c977345f50311349
nolanKeefe <npkeefe@udel.edu>
2025-03-02 00:33:07 -0500

    Starting navbar setting to change size of things

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -6,8 +6,26 @@ function Navigation(){
     return (
         <Navbar expand="lg" className="App-nav">
         <Navbar.Brand href="#home" class="page-logo"><img  height="50px" width="50px" src={logo} class="pixel-art"></img>Quest Log</Navbar.Brand>
+        <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
     </Navbar>
+    
     );
+    
 }
 
 export default Navigation;
